bec43a246c26d0317f62d7aae7e6b0c0121631cb
boing22222 <2652801845@qq.com>
2024-10-03 16:04:02 +0800

    character和player的一些参数

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -8,31 +8,124 @@ public class Character : MonoBehaviour
     void Start()
     {
         _animator = GetComponent<Animator>();
-        if (_animator != null) Debug.LogError(gameObject.name+"no animator!");
+        if (_animator != null) Debug.LogError(gameObject.name + "no animator!");
         _transform = transform;
-        if (_transform != null) Debug.LogError(gameObject+"no transfomation!");  
+        if (_transform != null) Debug.LogError(gameObject + "no transfomation!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     //注意动作和动画的联动
     //enimy、npc都可以从character派生，在character中可以完成一部分的工作
     protected virtual void Controller()
-    { 
+    {
         // do nothing;
         //control 根据不同具体角色去更改
 
     }
 
+    Transform _transform = null;
+    Animator _animator = null;
 
+    #region HP
+    [SerializeField] protected int hp;//角色hp
+    [SerializeField] protected int maxHP;
+    public int HP
+    {
+        set
+        {
+            hp = value;
+            if (hp > maxHP) HP = maxHP;
+            if (hp == 0)
+            {
+                //死亡
+            }
+            if (hp < 0) HP = 0;
+        }
+        get
+        {
+            return hp;
+        }
+    }
+    #endregion
 
-    [SerializeField]
-    private int hp;//角色hp
-    public float moveSpeed = 10.0f;
-    Transform _transform=null;
-    Animator _animator=null;
+    #region Money
+    [SerializeField] protected int money;//怪物设为掉落金钱量，角色为携带金钱量
+    public int Money
+    {
+        set
+        {
+            money = value;
+            if (money < 0) Money = 0;
+        }
+        get
+        {
+            return money;
+        }
+    }
+    #endregion
 
+    #region MoveSpeed
+    [SerializeField] protected float moveSpeed = 10.0f;
+    public float MoveSpeed
+    {
+        set
+        {
+            moveSpeed = value;
+            if (moveSpeed < 0) MoveSpeed = 0;
+        }
+        get
+        {
+            return moveSpeed;
+        }
+    }
+    #endregion
+
+    #region ImmuneTime
+    [SerializeField] protected float immuneTime;
+    public float ImmuneTime
+    {
+        set
+        {
+            immuneTime = value;
+            if (immuneTime < 0) ImmuneTime = 0;
+        }
+        get
+        {
+            return immuneTime;
+        }
+    }
+    #endregion
+
+    //public Bullet[] bulletList; 或者换成vector
+
+    #region Attack and Defence
+    [SerializeField] protected int defence;
+    [SerializeField] protected int attack;
+    public int Defence
+    {
+        set
+        {
+            defence = value;
+        }
+        get
+        {
+            return defence;
+        }
+    }
+    public int Attack
+    {
+        set
+        {
+            attack = value;
+        }
+        get
+        {
+            return attack;
+        }
+    }
+    #endregion
 }
--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : Character
@@ -7,14 +8,14 @@ public class Player : Character
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         if (rb == null) Debug.LogError("player no rigidbody2D!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void FixedUpdate()
     {
@@ -32,7 +33,7 @@ public class Player : Character
     {
         //移动状态机
         float horizontalF = Input.GetAxis(AxisMacro.HorizontalString);
-        float verticalF=Input.GetAxis(AxisMacro.VerticalString);
+        float verticalF = Input.GetAxis(AxisMacro.VerticalString);
         //根据状态移动角色
         switch (currStates)
         {
@@ -46,7 +47,7 @@ public class Player : Character
                 Debug.Log("player state is " + currStates);
                 break;
             case PlayerStates.Jump:
-                transform.Translate(new Vector3(0f,verticalF , 0f) * moveSpeed * Time.deltaTime*1000);
+                transform.Translate(new Vector3(0f, verticalF, 0f) * moveSpeed * Time.deltaTime * 1000);
                 //rb.AddForce(new Vector2(0, 10));
                 Debug.Log("player state is " + currStates);
                 break;
@@ -55,17 +56,172 @@ public class Player : Character
 
     //应该使用单例模式
     //只有一个player
-    private Player(){ }
-    private static Player player=null;
+    private Player() { }
+    private static Player player = null;
     private Rigidbody2D rb = null;
     //移动状态机变量
     enum PlayerStates
     {
         Idle, Move, Jump
     }
-    [SerializeField]
-    private int MaxJumpTimes = 1;
-    private PlayerStates currStates= PlayerStates.Idle;
+
+    private PlayerStates currStates = PlayerStates.Idle;
     private KeyCode JumpKeyCode = KeyCode.W;
+
+    #region MP
+    [SerializeField] private int mp;
+    [SerializeField] private int maxMP;
+    public int MP
+    {
+        set
+        {
+            mp = value;
+            if (mp > maxMP)
+            {
+                MP = maxMP;
+            }
+            if (mp < 0)
+            {
+                MP = 0;
+            }
+        }
+        get
+        {
+            return mp;
+        }
+    }
+    #endregion
+
+    #region Jump
+    [SerializeField] private float jumpHight;
+    [SerializeField] private float minJumpHight;
+    [SerializeField] private int maxJumpTimes = 1;
+    public float JumpHight
+    {
+        set
+        {
+            jumpHight = value;
+            if (jumpHight < minJumpHight) JumpHight = minJumpHight;
+        }
+        get
+        {
+            return jumpHight;
+        }
+    }
+    public int MaxJumpTimes
+    {
+        set
+        {
+            maxJumpTimes = value;
+        }
+        get
+        {
+            return maxJumpTimes;
+        }
+    }
+    #endregion
+
+    #region Luck
+    [SerializeField] private int luck;
+    public int Luck
+    {
+        set
+        {
+            luck = value;
+        }
+        get
+        {
+            return luck;
+        }
+    }
+    #endregion
+
+    #region Durity
+    [SerializeField] private int durity;
+    [SerializeField] private int maxDurity;
+    public int Durity
+    {
+        set
+        {
+            durity = value;
+            if (durity > maxDurity) Durity = maxDurity;
+            if (durity < 0) Durity = 0;
+        }
+        get
+        {
+            return durity;
+        }
+    }
+    #endregion
+
+    #region Dash
+    [SerializeField] private float dashDistance;
+    public float DashDistance
+    {
+        set
+        {
+            dashDistance = value;
+            if (dashDistance < 0) DashDistance = 0;
+        }
+        get
+        {
+            return dashDistance;
+        }
+    }
+    #endregion
+
+    #region Hate and Peace
+    [SerializeField] private int hate;
+    [SerializeField] private float peaceTime;
+    public int Hate
+    {
+        set
+        {
+            hate = value;
+        }
+        get
+        {
+            return hate;
+        }
+    }
+    public float PeaceTime
+    {
+        set
+        {
+            peaceTime = value;
+        }
+        get
+        {
+            return peaceTime;
+        }
+    }
+    #endregion
+
+    #region Hungry and Sanity
+    [SerializeField] private int hungry;
+    [SerializeField] private int sanity;
+    public int Hungry
+    {
+        set
+        {
+            hungry = value;
+        }
+        get
+        {
+            return hungry;
+        }
+    }
+    public int Sanity
+    {
+        set
+        {
+            sanity = value;
+        }
+        get
+        {
+            return sanity;
+        }
+    }
+    #endregion
 }
 
