acec9b1c4c0fc7de799e1b0ef4d6652295e88230
DEBUG-DONOT <877845066@qq.com>
2024-10-03 16:01:35 +0800

    道具测试

---
+++ Assets/Scripts/Items/DreivedItemFunction.cs
@@ -0,0 +1,56 @@
+/*
+ * 所有具体的功能在这里是实现
+ */
+
+//决定道具生效
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+
+/// <summary>
+/// //////////////////////////////////////////////////////
+/// 下面是具体生效的道具function
+/// //////////////////////////////////////////////////////
+/// </summary>
+using EffectItemFunction = ItemFunction;
+
+public class ChangeHP: EffectItemFunction
+{
+    //扣血
+    public override void Do_function(Item item)
+    {
+        
+    }
+}
+
+public class ChangeSpeed: EffectItemFunction
+{
+    //改移速
+    public override void Do_function(Item item)
+    {
+        
+    }
+}
+
+public class ChangeJumpHight : EffectItemFunction
+{
+    //跳跃高度
+    public override void Do_function(Item item)
+    {
+
+    }
+}
+
+public class SingleDirectionMove : EffectItemFunction
+{
+    public override void Do_function(Item item)
+    {
+        item.gameObject.transform.Translate(new UnityEngine.Vector3(1, 0, 0)*10);
+    }
+}
+
+
+
+
+
+
+
