d463350287412a719b452ff57aacd42923163bd6
王锴 <74810371+DEBUG-DONOT@users.noreply.github.com>
2024-10-05 15:41:49 +0800

    六边形生成终于完成了

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,73 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.UIElements;
 
 public class Cell : MonoBehaviour
 {
-    public  GameObject cell;
+    public  GameObject RealCell;
+    public GameObject virtualCell;
     // Start is called before the first frame update
     
     void Start()
     {
-        LeftPosition = new Vector3(1, 0, 0);
-        RightPosition=  new Vector3(-1,0, 0);
-        UpPosition=  new Vector3(0,1,0);
-        DownPosition=  new Vector3(0,1,0);
+        rendererSize=GetComponent<Renderer>().bounds.size.x*math.sqrt(3.0f)/2.0f;
+        //neighbors = new List<GameObject>(new GameObject[6]);
+    }
+    private void Awake()
+    {
+        neighbors = new List<GameObject>(new GameObject[6]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             nowSpwn = true;
-            //pauseGame();
+            //Player.GetInstance().canMove = false;
+            //Debug.Log("space");
+            GenerateVirtualCells();
+            pauseGame();
+            Debug.Log(this.gameObject.name + " space!");
         }
         if (nowSpwn)
         {
-            GameObject temp;
-            Vector3 spawnPosition=Vector3.zero;
-            if(Input.GetKeyDown (KeyCode.W))
-            { 
-                spawnPosition=UpPosition;
-                UpPosition += new Vector3(0, 1, 0);
-                //pauseGame();
-                Debug.Log("W");
-                temp = Instantiate(cell, spawnPosition + this.transform.position, transform.rotation);
-                temp.transform.parent = transform;
-                nowSpwn = false;
-            }
-            else if(Input.GetKeyDown (KeyCode.S))
+           //找到点击的位置
+           var clickedGO=CheckClick.CheckClickOnSomething();
+           if(clickedGO==null)
+           {
+                //Debug.Log("click nothing!");  
+           }
+           else if (virtualCell.CompareTag(clickedGO.tag)==true)
             {
-                spawnPosition = DownPosition;
-                DownPosition += new Vector3(0, -1, 0);
-                //pauseGame();
-                Debug.Log("S");
-                temp = Instantiate(cell, spawnPosition + this.transform.position, transform.rotation);
-                temp.transform.parent = transform;
-                nowSpwn = false;
-            }
-            else if(Input.GetKeyDown (KeyCode.D))
-            {
-                spawnPosition=LeftPosition;
-                LeftPosition += new Vector3(1, 0, 0);
-                //pauseGame();
-                Debug.Log("D");
-                temp = Instantiate(cell, spawnPosition + this.transform.position, transform.rotation);
-                temp.transform.parent = transform;
-                nowSpwn = false;
-            }
-            else if(Input.GetKeyDown (KeyCode.A))
-            {
-                spawnPosition=RightPosition;
-                RightPosition += new Vector3(-1, 0, 0);
-                //pauseGame();
-                Debug.Log("A");
-                temp = Instantiate(cell, spawnPosition + this.transform.position, transform.rotation);
-                temp.transform.parent = transform;
-                nowSpwn = false;
+                Vector3 pos=clickedGO.transform.position;   
+                DestroyAllVirtualCell();
+                GenerateRealCell(pos);
+                pauseGame();
             }
         }
+    }
+
+    public void DestroyAllVirtualCell()
+    {
+        GameObject[] allVirtualCells = GameObject.FindGameObjectsWithTag(virtualCell.tag);
+        foreach (GameObject virtualCell in allVirtualCells)
+        {
+            Destroy(virtualCell);
+        }
+    }
+    public void GenerateRealCell(Vector3 position)//在position位置生成一个real cell
+    {
+        GameObject temp = Instantiate(RealCell,  position, transform.rotation);
+        temp.transform.parent = transform;
+        nowSpwn = false;
+        //Debug.Log("generate real");
+        //将新生成的实例记录在数组中
+        var generateDirection = position - this.transform.position;
+        int index= HexagonDirection.GetIndex(generateDirection);
+        if (index == -1) Debug.LogError("can not to find index!");
+        else
+        {
+            this.neighbors[index] = temp;
+        }
+        //向六个方向判断并记录新生成的实例的周围的情况
+        Cell tempCell=temp.GetComponent<Cell>();
+        //for(int i=0;i<6;i++)
+        //{
+        //    RaycastHit2D hit = Physics2D.Raycast(temp.transform.position, HexagonDirection.Heax_Directions[i], rendererSize);
+        //    if(hit.collider!=null&&hit.collider.gameObject.CompareTag("Cell"))
+        //    {
+        //        tempCell.neighbors[i]= hit.collider.gameObject;
+        //        //Debug.Log("find sthd!");
+        //    }
+        //}
+
+    }
+
+    void GenerateVirtualCells()//生成所有可能的位置
+    {
+        Debug.Log(this.gameObject.name + "gen virtual");
+        for (int i = 0; i < 6; i++)
+        {
+            if (neighbors[i] == null)
+            {
+                ShowSingleVirtualCell(i);
+            }
+        }
+    }
+    void ShowSingleVirtualCell(int i)
+    {
+        RaycastHit2D hit2D = Physics2D.Raycast(HexagonDirection.Heax_Directions[i] + transform.position, Vector2.zero, 0.1f);
+        if (hit2D == true)
+        {
+            neighbors[i] = hit2D.collider.gameObject;
+            return; 
+        }
+        GameObject temp = Instantiate(virtualCell,HexagonDirection.Heax_Directions[i]+transform.position,transform.rotation);
+        temp.transform.parent = transform;
     }
     void pauseGame()
     {
@@ -88,7 +130,11 @@ public class Cell : MonoBehaviour
     {
         
     }
+    float rendererSize = 0;
     private bool gamePaused=false;
     private Vector3 LeftPosition,RightPosition,UpPosition,DownPosition;
     bool nowSpwn=false;
+    
+    public List<GameObject> neighbors ;
+    
 }
--- Assets/Scripts/Macro.cs
+++ Assets/Scripts/Macro.cs
@@ -4,6 +4,50 @@
  * animator.Set<Variable type>("variable name",variable value)
  * 类中的string存储与动画参数同名的变量，将变量的类型写在注释中
 */
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+public class HexagonDirection
+{
+    //public static Vector3 Heax_up = new Vector3(0, 1, 0);
+    static Vector3 Heax_left = new Vector3(-1, 0,0);
+    static Vector3 Heax_leftUp=new Vector3(-0.5f, 1);
+    static Vector3 Heax_leftDown = new Vector3(-0.5f, -1);
+    static Vector3 Heax_Right = new Vector3(1, 0);
+    static Vector3 Heax_RightDown = new Vector3(0.5f, -1);
+    static Vector3 Heax_RightUp = new Vector3(0.5f, 1);
+
+    public static int GetIndex(Vector3 position)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if(position==Heax_Directions[i]) return i;
+        }
+        return -1;
+    }
+    public static List<Vector3> Heax_Directions = new List<Vector3> {Heax_left,Heax_Right,Heax_leftUp,Heax_RightUp,Heax_leftDown,Heax_RightDown};
+
+}
+
+public class CheckClick
+{
+    public static GameObject CheckClickOnSomething()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 ray=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit=Physics2D.Raycast(ray,Vector2.zero);
+            if(hit.collider != null)
+            {
+                GameObject go=hit.collider.gameObject;
+                Debug.Log("click on"+go.name);
+                return go;
+            }
+            else return null;
+        }
+        else return null;
+    }
+}
 
 public class AxisMacro
 {
