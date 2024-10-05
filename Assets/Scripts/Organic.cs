2fcaea64ed148a31ef0e2b5f7301baf1c4cbc688
王锴 <74810371+DEBUG-DONOT@users.noreply.github.com>
2024-10-05 22:18:11 +0800

    有机物

--- Assets/Scripts/Organic.cs
+++ Assets/Scripts/Organic.cs
@@ -9,4 +9,34 @@ public class Organic : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        if (this.gameObject.CompareTag("Star"))
+        {
+
+        }
+        else if (this.gameObject.CompareTag("Weed"))
+        {
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject go=collision.gameObject;
+        //Èç¹ûÊÇmouth
+        if(go.CompareTag("Mouth"))
+        {
+            if(this.gameObject.CompareTag("Star"))
+            {
+                Player.GetInstance().Organic += 5;
+            }
+            else if(this.gameObject.CompareTag("Weed"))
+            {
+                Player.GetInstance().Organic += 1;
+            }
+        }
+    }
+
 }
