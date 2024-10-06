15df63aa60b28dd9fc9e554038cfd28722336764
王锴 <74810371+DEBUG-DONOT@users.noreply.github.com>
2024-10-06 15:57:15 +0800

    修复了双空格bug

--- Assets/Scripts/EnemyGenerate.cs
+++ Assets/Scripts/EnemyGenerate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyGenerate : MonoBehaviour
 {
+    public GameObject enemyGO;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,14 +12,21 @@ public class EnemyGenerate : MonoBehaviour
     }
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+      //  rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }   
-
-    Rigidbody2D rigidbody = null;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for(int i = 0;i<EnemyNumber;i++)
+        {
+            Instantiate(enemyGO,transform.position+ new Vector3(0f,1f,0f),transform.rotation);
+        }
+    }
+    public int EnemyNumber = 1;
+    //Rigidbody2D rigidbody = null;
 }
